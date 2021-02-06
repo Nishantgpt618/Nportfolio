f4e5496535b4e3e2621331a4b8c102fba34d18cd
Nishantgpt618 <nishantgpt618@gmail.com>
2021-02-06 21:41:47 +0530

    Updates

---
+++ precache-manifest.b010061e97a54da5728ade675efd378a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af804cbfed38e939455a2850c7a152f6",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "2770248448bd347bf282",
+    "url": "/Portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3e5a9decae3b5e7065a4",
+    "url": "/Portfolio/static/css/main.2e4de44d.chunk.css"
+  },
+  {
+    "revision": "2770248448bd347bf282",
+    "url": "/Portfolio/static/js/2.ed017d29.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/Portfolio/static/js/2.ed017d29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e5a9decae3b5e7065a4",
+    "url": "/Portfolio/static/js/main.76ec080b.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "5ffe3e02f6c399427ae17130f8cbe1b2",
+    "url": "/Portfolio/static/media/Instagram.5ffe3e02.png"
+  },
+  {
+    "revision": "f3155f6686cfa52fea39a8349a24e98f",
+    "url": "/Portfolio/static/media/Linkedin.f3155f66.png"
+  },
+  {
+    "revision": "3a8d845b1de79c8b6142d10540903de2",
+    "url": "/Portfolio/static/media/Resume-Nishant-Gupta+91-8077220024-Email-nishantgupta6688@gmail.com-Location-Bareilly,Uttar-pradesh.3a8d845b.pdf"
+  },
+  {
+    "revision": "7361c6cc17463999a1a3646d082b56af",
+    "url": "/Portfolio/static/media/me1.7361c6cc.png"
+  },
+  {
+    "revision": "8cf6df46348905354cf8f6de20714182",
+    "url": "/Portfolio/static/media/profile.8cf6df46.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Nportfolio/precache-manifest.033c17836487fc2b70edb323daed27c7.js"
+  "/Portfolio/precache-manifest.b010061e97a54da5728ade675efd378a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Nportfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
